237f1aba4cf0799a41a48008b35521459a640aba
MayurMansukh <Mansukhmayur99@gmail.com>
2021-03-11 00:09:51 +0530

    add more then 3 parameter

--- src/main/java/GenericUc/GenericUcMax.java
+++ src/main/java/GenericUc/GenericUcMax.java
@@ -14,16 +14,8 @@ public class GenericUcMax<T extends Comparable<T>> {
 
     public <T extends Comparable<T>> T maximum()
     {
-        T max;
         Arrays.sort(items);
-        max= (T) items[items.length-1];
-        printMaximum(max);
-        return max;
-    }
-
-    public static <T> void printMaximum(T max)
-    {
-        System.out.println("Maximum is:  "+max);
+        return (T) items[items.length-1];
     }
 
 
